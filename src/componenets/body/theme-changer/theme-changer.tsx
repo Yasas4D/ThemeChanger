0ccfdb449accd2cd0d4717a57a6acae06e6e65bb
Yasas Sandeepa <yasassandeepa007@gmail.com>
2020-11-12 23:02:41 +0530

    Add theme changer componenet

---
+++ src/componenets/body/theme-changer/theme-changer.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./themeChanger.css";
+
+export const ThemeChanger = () => {
+  return (
+    <div>
+      <div className="sub-main">
+        <button className="button-three">Click Me</button>
+      </div>
+    </div>
+  );
+};
